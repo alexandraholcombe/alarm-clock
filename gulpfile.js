f6cb953113ac5e9b65817e0ce2a5300b12bd6dd5
Alexandra Holcombe and Lawrence Eby <alexandra+lawrence@>
2017-03-14 08:44:14 -0700

    add browserify task, concat task, minify task, build task, and clean task

--- gulpfile.js
+++ gulpfile.js
@@ -19,3 +19,35 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require('browser-sync').create();
+
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js']})
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src('./build/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
